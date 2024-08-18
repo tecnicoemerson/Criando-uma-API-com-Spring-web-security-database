631376b6e60c3070c0cd2cfd5476ec78b8e38568
tecnicoemerson <emersonrodrigues5578@gmail.com>
2024-08-17 21:30:22 -0300

    Adicionando Segurança a uma API REST com Spring Security

---
+++ src/main/java/emerson/spring/security/EmersonSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package emerson.spring.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmersonSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmersonSpringSecurityApplication.class, args);
+	}
+
+}
